fe377d352f23136211952054da5346b41ac39553
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-03-17 20:12:38 +0100

    sym2 its amaizing

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -8,12 +8,20 @@ class DefaultController extends Controller
 {
     public function indexAction($count, $firstName)
     {
+//        $em = $this->container->get('doctrine')->getManager();
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
+
+        $event = $repo->findOneBy(array(
+           'name' => 'Darth surprise birthday!'
+        ));
+
         return $this->render(
             'EventBundle:Default:index.html.twig',
             array(
                 'count' => $count,
                 'name' => $firstName,
-                'ackbar' => "Its a traaaaaaaaaaaaaaap..."
+                'event' => $event
             )
         );
     }
